076716e1fa66c57f854d82a16f9e3880a095115b
Chandan Singh <contact.chandu13@gmail.com>
2026-08-11 16:10:41 +0530

    Update initKoinForIOS.kt

--- shared/src/iosMain/kotlin/com/app/orientalinsurance/koin/initKoinForIOS.kt
+++ shared/src/iosMain/kotlin/com/app/orientalinsurance/koin/initKoinForIOS.kt
@@ -2,7 +2,8 @@
 package com.app.orientalinsurance.koin
 
 import com.app.orientalinsurance.data.multiplateformData.iosModule
-
+@OptIn(ExperimentalObjCName::class)
+@ObjCName("initKoinForIOS")
 fun initKoinForIOS() {
 
     initKoin(
